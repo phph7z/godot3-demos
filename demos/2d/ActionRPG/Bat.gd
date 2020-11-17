ceb22a90ff10fdbfa937fcf23dfea1bd417a3859
youkabeng <phph1990@outlook.com>
2020-11-17 20:57:59 +0800

    start to add bat AI

--- demos/2d/ActionRPG/Bat.gd
+++ demos/2d/ActionRPG/Bat.gd
@@ -1,14 +1,37 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
 
 const DeathEffect = preload("res://EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = CHASE
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, 200 * delta)
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			pass
 
 
 func _on_HurtBox_area_entered(area):
