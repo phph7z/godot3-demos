b6e505306706fc9791128e8b2f97541597ddbf92
youkabeng <phph1990@outlook.com>
2021-02-11 22:08:45 +0800

    update camera control

--- demos/3d/FoodFight/Scenes/Characters/Players/Player.gd
+++ demos/3d/FoodFight/Scenes/Characters/Players/Player.gd
@@ -3,28 +3,61 @@ extends KinematicBody
 const SPEED = 10
 var motion = Vector3()
 var movement_state = 0 # idle is 0, run is 1
+export var mouse_sensitivity = 600
 
 const MIN_BLEND_SPEED = 0.125 # minimum movement threshold before we start to blend animations
 const BLEND_TO_RUN = 0.075
 const BLEND_TO_IDLE = 0.1
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta):
 	move()
 	animate()
 
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation = h_camera_rotation(-event.relative.x/mouse_sensitivity)
+		$Camera.rotation = v_camera_rotation(-event.relative.y/mouse_sensitivity)
+
+
+func h_camera_rotation(camera_rotation):
+	return rotation + Vector3(0, camera_rotation, 0)
+
+
+func v_camera_rotation(camera_rotation):
+	var rot = $Camera.rotation + Vector3(camera_rotation, 0, 0)
+	rot.x = clamp(rot.x, PI/-8, PI/8)
+	return rot
+
 func move():
-	var x = Input.get_action_strength("forward") - Input.get_action_strength("backward")
-	var z = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var movement_dir = get_2d_movement_dir()
+	var direction = Vector3.ZERO
+	var camera_xform = $Camera.global_transform
 	
-	motion = Vector3(x,0,z)
-	if not motion == Vector3(0,0,0):
-		face_forward(x,z)
+	direction -= camera_xform.basis.z.normalized() * movement_dir.x
+	direction += camera_xform.basis.x.normalized() * movement_dir.y
+	direction.y = 0
+	
+	motion = direction
 	
 	move_and_slide(motion * SPEED, Vector3(0,1,0))
 
+func get_2d_movement_dir():
+	var x = Input.get_action_strength("forward") - Input.get_action_strength("backward")
+	var y = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	var movement_dir = Vector2(x,y)
+	if not movement_dir == Vector2.ZERO:
+		face_forward(x,y)
+	
+	return movement_dir.normalized()
+
 
 func face_forward(x,z):
-	rotation.y = atan2(x, z) - PI*2
+	$Armature.rotation.y = atan2(x, z) - PI/2
 
 
 func animate():
