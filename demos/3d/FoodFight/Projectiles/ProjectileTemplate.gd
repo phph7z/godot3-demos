9a5c8da1e2947b38c93d2c69024067abb0ecc4a5
youkabeng <phph1990@outlook.com>
2021-03-03 21:16:00 +0800

    finish adding bystanders

--- demos/3d/FoodFight/Projectiles/ProjectileTemplate.gd
+++ demos/3d/FoodFight/Projectiles/ProjectileTemplate.gd
@@ -5,3 +5,9 @@ func _ready():
 
 func _on_Timer_timeout():
 	queue_free()
+
+
+func _on_ProjectTemplate_body_shape_entered(body_id, body, body_shape, local_shape):
+	if body.has_method("hurt"):
+		body.hurt()
+		queue_free()
